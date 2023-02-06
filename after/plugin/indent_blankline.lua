c4d4eac9bd0f71d559f20b732d0dd734fa685524
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-06 13:18:31 -0500

    Changed indent blankline configs.

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -5,12 +5,16 @@ vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
 require("indent_blankline").setup {
-  show_end_of_line = true,
+  show_end_of_line = false,
   space_char_blankline = " ",
   show_current_context = false,
-  show_current_context_start = true,
-  indent = 2,
+  show_current_context_start = false,
+  indent = 1,
   char_highlight_list = {
     "IndentBlanklineIndent1",
     "IndentBlanklineIndent2",
