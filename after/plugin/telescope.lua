b36141d882a0d829322fc5205d172feebf336561
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-06 13:17:54 -0500

    Renamed telescope maps.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,11 @@
-local m = require"ul.maps"
+local m = require "ul.maps"
 
 m.map("n", "<c-p>", "<CMD>Telescope find_files<CR>")
 m.map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
-m.map("n", "<leader>ft", "<CMD>Telescope live_grep<CR>")
+m.map("n", "<c-f>", "<CMD>Telescope live_grep<CR>")
+m.map("n", "<leader>fp", "<CMD>Telescope live_grep<CR>")
 
-require('telescope').setup{
+require('telescope').setup {
   defaults = {
     file_ignore_patterns = { "node_modules", ".git" },
   }
