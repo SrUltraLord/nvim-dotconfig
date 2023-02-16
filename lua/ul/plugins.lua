b5c83a07b3e414b0b9155f5607d2677aba5256e5
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-16 17:55:35 -0500

    Replaced bar bar w/ buffer line.

--- lua/ul/plugins.lua
+++ lua/ul/plugins.lua
@@ -21,36 +21,7 @@ return packer.startup(function(use)
           "glepnir/dashboard-nvim",
           event = "VimEnter",
           config = function()
-            require('dashboard').setup {
-                theme = 'hyper',
-                config = {
-                    week_header = {
-                        enable = true,
-                    },
-                    shortcut = {
-                        { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
-                        {
-                            desc = ' Files',
-                            group = 'Label',
-                            action = 'Telescope find_files',
-                            key = 'f',
-                        },
-                        {
-                            desc = ' Apps',
-                            group = 'DiagnosticHint',
-                            action = 'Telescope app',
-                            key = 'a',
-                        },
-                        {
-                            desc = ' dotfiles',
-                            group = 'Number',
-                            action = 'Telescope dotfiles',
-                            key = 'd',
-                        },
-                    },
-                },
-                -- config
-            }
+            require('dashboard').setup {}
           end,
           requires = {
               { 'nvim-tree/nvim-web-devicons' }
@@ -109,7 +80,14 @@ return packer.startup(function(use)
       }
       use { 'simrat39/rust-tools.nvim' } -- Rust LSP
 
-      use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
+      use {
+          'akinsho/bufferline.nvim',
+          tag = "v3.*",
+          requires = 'nvim-tree/nvim-web-devicons',
+          config = function()
+            require("bufferline").setup()
+          end,
+      }
 
       use {
           'numToStr/Comment.nvim',
