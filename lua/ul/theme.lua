67adbe7073abc30516a595e1d38103ce683f6575
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-16 17:55:52 -0500

    Added transparent bg option.

--- lua/ul/theme.lua
+++ lua/ul/theme.lua
@@ -1,7 +1,15 @@
-local ok, _ = pcall(vim.cmd, "colorscheme catppuccin")
+local ok, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
+
+-- Makes bg transparent
+vim.cmd [[
+  autocmd SourcePost * highlight Normal     ctermbg=NONE guibg=NONE
+              \ |    highlight LineNr     ctermbg=NONE guibg=NONE
+              \ |    highlight SignColumn ctermbg=NONE guibg=NONE
+]]
+
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({ higroup = "Visual", timeout = 120 })
-  end,
+    callback = function()
+      vim.highlight.on_yank({ higroup = "Visual", timeout = 120 })
+    end,
 })
