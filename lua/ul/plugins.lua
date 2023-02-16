9141819624cc46330184af4f35f208b5ca29b521
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-16 18:40:53 -0500

    Changed format.

--- lua/ul/plugins.lua
+++ lua/ul/plugins.lua
@@ -13,142 +13,133 @@ vim.cmd([[
 ]])
 
 return packer.startup(function(use)
-      use "wbthomason/packer.nvim"
+  use "wbthomason/packer.nvim"
 
-      -- Theme
-      use { "catppuccin/nvim", as = "catppuccin" }
-      use {
-          "glepnir/dashboard-nvim",
-          event = "VimEnter",
-          config = function()
-            require('dashboard').setup {}
-          end,
-          requires = {
-              { 'nvim-tree/nvim-web-devicons' }
-          },
+  -- Theme
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use {
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require('dashboard').setup {}
+    end,
+    requires = { { 'nvim-tree/nvim-web-devicons' } },
 
-      }
+  }
 
-      -- Sessions
-      use({
-          "folke/persistence.nvim",
-          event = "BufReadPre", -- this will only start session saving when an actual file was opened
-          module = "persistence",
-          config = function()
-            require("persistence").setup()
-          end,
-      })
+  -- Sessions
+  use({
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup()
+    end,
+  })
 
-      -- Status line
-      use {
-          'nvim-lualine/lualine.nvim',
-          requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-      }
-      use "kyazdani42/nvim-web-devicons"
+  -- Status line
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use "kyazdani42/nvim-web-devicons"
 
-      -- Floating terminal
-      use "voldikss/vim-floaterm"
+  -- Floating terminal
+  use "voldikss/vim-floaterm"
 
-      -- Git
-      use { 'tanvirtin/vgit.nvim', requires = 'nvim-lua/plenary.nvim' }
-      use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-      use "nvim-lua/plenary.nvim"
+  -- Git
+  use { 'tanvirtin/vgit.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use "nvim-lua/plenary.nvim"
 
-      -- File editing
-      use { "gbprod/yanky.nvim" }
-      use { "petertriho/nvim-scrollbar" }
-      use {
-          'VonHeikemen/lsp-zero.nvim',
-          requires = {
-              -- LSP Support
-              { 'neovim/nvim-lspconfig' },
-              { 'williamboman/mason.nvim' },
-              { 'williamboman/mason-lspconfig.nvim' },
+  -- File editing
+  use { "gbprod/yanky.nvim" }
+  use { "petertriho/nvim-scrollbar" }
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-              -- Autocompletion
-              { 'hrsh7th/nvim-cmp' },
-              { 'hrsh7th/cmp-buffer' },
-              { 'hrsh7th/cmp-path' },
-              { 'saadparwaiz1/cmp_luasnip' },
-              { 'hrsh7th/cmp-nvim-lsp' },
-              { 'hrsh7th/cmp-nvim-lua' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
-              -- Snippets
-              { 'L3MON4D3/LuaSnip' },
-              { 'rafamadriz/friendly-snippets' },
-          }
-      }
-      use { 'simrat39/rust-tools.nvim' } -- Rust LSP
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  }
+  use { 'simrat39/rust-tools.nvim' } -- Rust LSP
 
-      use {
-          'akinsho/bufferline.nvim',
-          tag = "v3.*",
-          requires = 'nvim-tree/nvim-web-devicons',
-          config = function()
-            require("bufferline").setup()
-          end,
-      }
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "v3.*",
+    requires = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup()
+    end,
+  }
 
-      use {
-          'numToStr/Comment.nvim',
-          config = function()
-            require('Comment').setup()
-          end
-      }
-      use {
-          "utilyre/barbecue.nvim",
-          requires = {
-              "neovim/nvim-lspconfig",
-              "smiteshp/nvim-navic",
-              "kyazdani42/nvim-web-devicons", -- optional
-          },
-          config = function()
-            require("barbecue").setup()
-          end,
-      }
-      use { 'sQVe/sort.nvim' }
-      use {
-          "folke/trouble.nvim",
-          requires = "kyazdani42/nvim-web-devicons",
-          config = function()
-            require("trouble").setup {
-                auto_preview = true,
-                use_diagnostic_signs = true
-            }
-          end
-      }
-      use { 'jiangmiao/auto-pairs' }
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+  use {
+    "utilyre/barbecue.nvim",
+    requires = {
+      "neovim/nvim-lspconfig",
+      "smiteshp/nvim-navic",
+      "kyazdani42/nvim-web-devicons", -- optional
+    },
+    config = function()
+      require("barbecue").setup()
+    end,
+  }
+  use { 'sQVe/sort.nvim' }
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup()
+    end
+  }
+  use { 'jiangmiao/auto-pairs' }
 
-      use {
-          "folke/todo-comments.nvim",
-          requires = "nvim-lua/plenary.nvim",
-          config = function()
-            require("todo-comments").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-          end
-      }
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup()
+    end
+  }
 
-      -- File Mgmt
-      use "nvim-tree/nvim-tree.lua"
-      use { "nvim-telescope/telescope.nvim",
-          tag = "0.1.0",
-          requires = { { "nvim-lua/plenary.nvim" } },
-      }
-      use "nvim-telescope/telescope-file-browser.nvim"
+  -- File Mgmt
+  use "nvim-tree/nvim-tree.lua"
+  use { "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  }
+  use "nvim-telescope/telescope-file-browser.nvim"
 
-      -- File Decorations
-      use "lukas-reineke/indent-blankline.nvim"
+  -- File Decorations
+  use "lukas-reineke/indent-blankline.nvim"
 
-      -- Syntax Highlighting
-      use "nvim-treesitter/nvim-treesitter"
+  -- Syntax Highlighting
+  use "nvim-treesitter/nvim-treesitter"
 
-      -- Misc.
-      use 'andweeb/presence.nvim'
+  -- Misc.
+  use 'andweeb/presence.nvim'
 
-      if packer_bootstrap then
-        packer.sync()
-      end
-    end)
+  if packer_bootstrap then
+    packer.sync()
+  end
+end)
