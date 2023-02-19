38950cb8ec668043fba3551a30b89f1d887f7014
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-19 10:52:29 -0500

    Updated theme

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -1,1 +1,5 @@
 vim.g.neovide_refresh_rate = 120
+
+if vim.g.neovide then
+  vim.opt.guifont = { "DankMono NF", "h15" } -- text below applies for VimScript
+end
--- lua/ul/theme.lua
+++ lua/ul/theme.lua
@@ -1,11 +1,11 @@
 local ok, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
 
 -- Makes bg transparent
-vim.cmd [[
-  autocmd SourcePost * highlight Normal     ctermbg=NONE guibg=NONE
-              \ |    highlight LineNr     ctermbg=NONE guibg=NONE
-              \ |    highlight SignColumn ctermbg=NONE guibg=NONE
-]]
+-- vim.cmd [[
+--   autocmd SourcePost * highlight Normal     ctermbg=NONE guibg=NONE
+--               \ |    highlight LineNr     ctermbg=NONE guibg=NONE
+--               \ |    highlight SignColumn ctermbg=NONE guibg=NONE
+-- ]]
 
 
 vim.api.nvim_create_autocmd("TextYankPost", {
