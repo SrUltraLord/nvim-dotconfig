203c4a18fc730b045925ae9b5288b0406ed1fac2
SrUltraLord <dareyes6@espe.edu.ec>
2023-02-28 17:09:30 -0500

    Fixed formatting.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,11 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-    'html',
-    'eslint',
-    'tsserver',
-    'volar',
-    'rust_analyzer'
+  'html',
+  'eslint',
+  'tsserver',
+  'volar',
+  'rust_analyzer'
 })
 
 local rust_lsp = lsp.build_options('rust_analyzer', {})
