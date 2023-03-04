0cfc1145f80ee7b05c88056efdf3f3a425741813
SrUltraLord <dareyes6@espe.edu.ec>
2023-03-03 22:38:00 -0500

    Added neovide animations.

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -2,6 +2,7 @@ if vim.g.neovide then
   vim.g.neovide_refresh_rate = 60
   vim.opt.guifont = { "DankMono NF", "h12" }
   vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
 
   local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
