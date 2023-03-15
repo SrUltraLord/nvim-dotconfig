39c2228ae1228de613417bb519b3f9ceae92b820
SrUltraLord <dareyes6@espe.edu.ec>
2023-03-15 10:13:15 -0500

    Added map for telescope hidden files.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local m = require "ul.maps"
 
 m.map("n", "<c-p>", "<CMD>Telescope find_files<CR>")
-m.map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
+m.map("n", "<leader>ff", "<CMD>Telescope find_files hidden=true<CR>")
 m.map("n", "<c-f>", "<CMD>Telescope live_grep<CR>")
 m.map("n", "<leader>fp", "<CMD>Telescope live_grep<CR>")
 
